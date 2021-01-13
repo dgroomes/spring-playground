349173a4736b3a9d59c87cd5ad21f41ac96dc0ee
David Groomes <dwgroomes@gmail.com>
2021-01-12 21:30:09 -0600

    Config file

---
+++ config/src/main/java/dgroomes/config/FortuneService.java
@@ -0,0 +1,26 @@
+package dgroomes.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+/**
+ *  A toy example that requires a file for configuration. This class is a vehicle to show Spring's configuration
+ *  features with regard to files.
+ */
+@Service
+public class FortuneService {
+
+    private static final Logger log = LoggerFactory.getLogger(FortuneService.class);
+
+    public FortuneService(@Qualifier("fortunesConfiguration") File config) throws IOException {
+        log.info("Configuration file: {}", config);
+        String configContent = Files.readString(config.toPath());
+        log.info("Configuration content:\n{}", configContent);
+    }
+}
--- config/src/main/java/dgroomes/config/Main.java
+++ config/src/main/java/dgroomes/config/Main.java
@@ -8,17 +8,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.io.File;
+
 @SpringBootApplication
 public class Main {
 
     private static final Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
+        log.info("Current directory: {}", System.getProperty("user.dir"));
         SpringApplication.run(Main.class, args);
     }
 
     @Bean
     public ApplicationRunner runner(@Value("${app.message}") String message) {
         return args -> log.info(message);
+    }
+
+    @Bean("fortunesConfiguration")
+    public File fortunesConfiguration() {
+        return new File("fortunes.txt");
     }
 }
