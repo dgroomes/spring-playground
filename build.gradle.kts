5f4c1c967fcf383e692ad7116941b717d6d44bbb
David Groomes <dwgroomes@gmail.com>
2021-01-23 11:49:48 -0600

    Upgrade to Gradle 6.8.1; 'assembleAll' task

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+/**
+ * A convenience task to assemble all of the included projects. This is useful as a quick smoke test of the Gradle config
+ * and a general environmental test.
+ */
+tasks.register("assembleAll") {
+    dependsOn(gradle.includedBuild("caching").task(":assemble"))
+    dependsOn(gradle.includedBuild("scheduling").task(":assemble"))
+    dependsOn(gradle.includedBuild("config").task(":assemble"))
+}
