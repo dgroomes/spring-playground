9aeae62f3d710ef07199031ea5bdb63ab18d320f
David Groomes <dwgroomes@gmail.com>
2021-01-23 12:51:30 -0600

    Extract beans into its own class
    * Avoid awkward bean for the "main" method

--- bootless/src/main/java/dgroomes/springplayground/bootless/Main.java
+++ bootless/src/main/java/dgroomes/springplayground/bootless/Main.java
@@ -1,21 +1,28 @@
 package dgroomes.springplayground.bootless;
 
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
 public class Main {
 
     private static final Logger log = org.slf4j.LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
+        log.info("Wiring up a simple Spring application context");
         var context = new AnnotationConfigApplicationContext(Main.class.getPackageName());
 
         var sayHello = context.getBean("sayHello", Runnable.class);
         sayHello.run();
     }
+}
+
+@Configuration
+class Beans {
+
+    private static final Logger log = LoggerFactory.getLogger(Beans.class);
 
     @Bean
     public Runnable sayHello() {
